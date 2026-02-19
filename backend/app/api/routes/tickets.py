0bd354098043f4cb62b7f7d0568916dee8bb7b44
AlanFC253 <ala253523@live.com>
2026-02-19 03:29:53 -0300

    feat(tickets): complete CRUD endpoints

--- backend/app/api/routes/tickets.py
+++ backend/app/api/routes/tickets.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends , HTTPException
 from sqlalchemy.orm import Session
 
 from app.db.session import get_db
 from app.models.ticket import Ticket
-from app.schemas.ticket import TicketCreate, TicketOut
+from app.schemas.ticket import TicketCreate, TicketOut, TicketUpdate
 
 router = APIRouter(prefix="/tickets",tags=["ticket"])
 
@@ -26,3 +26,40 @@ def create_ticket(payload: TicketCreate,db: Session = Depends(get_db)):
 def list_tickets(db: Session=Depends(get_db)):
     tickets = db.query(Ticket).all()
     return tickets
+
+@router.get("/{id}", response_model=TicketOut)
+def get_id(id: int, db: Session=Depends(get_db)):
+    ticket = db.get(Ticket,id)
+
+    if not ticket:
+        raise HTTPException(status_code=404, detail="Ticket not found") 
+    return ticket
+
+@router.patch("/{id}", response_model=TicketOut)
+def update_ticket(id:int,payload:TicketUpdate, db: Session = Depends(get_db)):
+    ticket = db.get(Ticket,id)
+
+    if not ticket:
+        raise HTTPException(status_code=404,detail="Ticket not found")
+    
+    update_data = payload.model_dump(exclude_unset=True)
+
+    for field,value in update_data.items():
+        setattr(ticket,field,value)  
+
+    db.commit()
+    db.refresh(ticket)
+    return ticket
+
+@router.delete("/{id}",status_code=204)
+def delete_ticket(id:int,db: Session=Depends(get_db)):
+
+    ticket = db.get(Ticket,id)
+
+    if not ticket:
+        raise HTTPException(status_code=404,detail="Ticket not found")
+    
+    db.delete(ticket)
+    db.commit()
+
+    return
